17bb4c768365512625489c01eec0efd7fba0b4e3
Cris Mel <cristian.melian12@gmail.com>
2025-05-15 21:10:37 -0400

    Objeto arriendo

---
+++ src/sumativa_4/Arriendo.java
@@ -0,0 +1,46 @@
+package sumativa_4;
+
+public class Arriendo {
+	
+	// Atributos
+	private int numArriendo;
+	private String fecArr; // fecha de arriendo
+	private int diasArriendo;
+	
+	// Constructor
+	public Arriendo(int pNumArriendo, String pFecArr, int pDiasArriendo) {
+		this.numArriendo = pNumArriendo;
+		this.fecArr = pFecArr;
+		this.diasArriendo = pDiasArriendo;
+	}
+	
+	// Getters
+	public int getNumArriendo() {
+		return numArriendo;
+	}
+	public String getFecArr() {
+		return fecArr;
+	}
+	public int getDiasArriendo() {
+		return diasArriendo;
+	}
+	
+	// Setters
+	public void setNumArriendo(int pNumArriendo) {
+		this.numArriendo = pNumArriendo;
+	}
+	public void setFecArr(String pFecArr) {
+		this.fecArr = pFecArr;
+	}
+	public void setDiasArriendo(int pDiasArriendo) {
+		this.diasArriendo = pDiasArriendo;
+	}
+	
+	// Método toString
+	@Override
+	public String toString() {
+		return "Arriendo [número:" + numArriendo + ", fecha:" + fecArr + ", dias de arriendo:" + diasArriendo + "]";
+	}
+	
+	// Métodos customers
+}
