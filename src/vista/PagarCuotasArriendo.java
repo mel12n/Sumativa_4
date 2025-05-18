23823f1f907e0fe9daf2a6b810f94f932fab9997
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2025-05-18 19:40:19 -0400

    feat: plug controller to Pagar Cuotas Arriendo

--- src/vista/PagarCuotasArriendo.java
+++ src/vista/PagarCuotasArriendo.java
@@ -1,5 +1,6 @@
 package vista;
 
+import controlador.ControladorVistaPagarCuotasArriendo;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -22,7 +23,6 @@ public class PagarCuotasArriendo extends JPanel {
     public PagarCuotasArriendo() {
         setLayout(null);
         setBackground(new Color(250, 245, 240));
-
         inicializarComponentes();
     }
 
@@ -91,6 +91,7 @@ public class PagarCuotasArriendo extends JPanel {
         add(btnRealizarPago);
     }
 
+    // Getters para componentes
     public JComboBox<String> getCmbClientes() {
         return cmbClientes;
     }
@@ -127,8 +128,11 @@ public class PagarCuotasArriendo extends JPanel {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame f = new JFrame();
-            new PagarCuotasArriendo().crearGUI(f);
+            PagarCuotasArriendo view = new PagarCuotasArriendo();
+            JFrame frame = new JFrame();
+            view.crearGUI(frame);
+            // Asociar el controlador a la vista
+            new ControladorVistaPagarCuotasArriendo(view);
         });
     }
 }
