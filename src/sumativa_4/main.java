af5d64efe175af71330da9ba1a147f2d73240f96
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2025-05-18 19:05:39 -0400

    use incoming changes

--- src/sumativa_4/main.java
+++ src/sumativa_4/main.java
@@ -1,42 +1,27 @@
 package sumativa_4;
 
+import controlador.ControladorVistaClientes;
 import java.util.ArrayList;
-
 import javax.swing.JFrame;
-
 import vista.Clientes;
 
-import controlador.ControladorVistaClientes;
-
 //import controlador.ControladorVistaClientes;
-
 //import controlador.Controlador;
-
-
 public class main {
 
-	//Main para probar la vista AgregarCliente
-		public static void main(String[] args) {
-<<<<<<< HEAD
-	        JFrame ventana = new JFrame("Arriendo con Cuotas");
-	        ArriendoConCuotas vista = new ArriendoConCuotas();
-	        vista.crearGUI(ventana);
-	        ControladorVistaArriendo controlador = new ControladorVistaArriendo(vista);
-	        ventana.setLocationRelativeTo(null); // Centra la ventana
-=======
-			
-			AbrirDocumento abrirDocumento = new AbrirDocumento(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-			abrirDocumento.cargarListaClientes();
-			abrirDocumento.cargarListaArriendos();
-			abrirDocumento.cargarListaAutomoviles();
-			
-	        			
-			JFrame ventana = new JFrame("Agregar Cliente");
-	        Clientes vistaClientes = new Clientes();
-	        vistaClientes.crearGUI(ventana);
-	        ControladorVistaClientes controlador = new ControladorVistaClientes(vistaClientes);
-	        ventana.setLocationRelativeTo(null); 
->>>>>>> branch 'master' of https://github.com/mel12n/Sumativa_4
-		}
+    //Main para probar la vista AgregarCliente
+    public static void main(String[] args) {
+
+        AbrirDocumento abrirDocumento = new AbrirDocumento(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        abrirDocumento.cargarListaClientes();
+        abrirDocumento.cargarListaArriendos();
+        abrirDocumento.cargarListaAutomoviles();
+
+        JFrame ventana = new JFrame("Agregar Cliente");
+        Clientes vistaClientes = new Clientes();
+        vistaClientes.crearGUI(ventana);
+        ControladorVistaClientes controlador = new ControladorVistaClientes(vistaClientes);
+        ventana.setLocationRelativeTo(null);
+    }
 
 }
